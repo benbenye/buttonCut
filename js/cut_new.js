119350c83ab8877500d0c70c4d2bf63566d63f4d
benbenye <1203846281@qq.com>
2015-01-25 00:54:04 +0800

    cut_new

--- js/cut_new.js
+++ js/cut_new.js
@@ -33,16 +33,7 @@ $(function(){
 			this.btnArr[i] = new Array();
 		}
 
-		// this.btnArr.forEach(function(that){
-		// 	that = new Array(this.dimensionVertical);
-		// 	that.forEach(function(_that){
-		// 		// 随机棋盘
-		// 		_that = Math.floor(Math.random() * this.colors);
-		// 	});
-		// });
-
-		// 渲染到dom
-			// <li id="l04"><span class="r"></span></li>
+		// 渲染到dom,<li id="l04"><span class="r"></span></li>
 		var _temDom = '';
 		for(var i = 0; i < this.dimensionVertical; ++i){
 			for(var j = 0; j <this.dimensionHorizon; ++j){
@@ -59,24 +50,80 @@ $(function(){
 		$('#mainUl').html(_temDom);
 	};
 
-	var bo = new BtnBoard(5,5,5,$(window).width());
+	var bo = new BtnBoard(8,4,5,$(window).width());
 	bo._init();
 
 	$('.btn').on('click',function(){
 		var i = $(this).index(),
 			x = i % bo.dimensionHorizon,
-			y = i / bo.dimensionHorizon;
+			y = i / bo.dimensionHorizon,
+			btnObj = {
+				x : i % bo.dimensionHorizon,
+				y : Math.floor(i / bo.dimensionHorizon),
+				color : bo.btnArr[x][y]
+			};
 
 		if(bo.fromTo.length === 0){
 			// 新游戏
-			bo.fromTo.push();
+			bo.fromTo.push(btnObj);
+			// addSeleClass();
+			$(this).addClass('active');
+		}else{
+			// if the color is same
+			if(bo.fromTo[0].color === btnObj.color){
+				if(bo.fromTo[0].x === btnObj.x){
+					var _tempArr = [];
+					_tempArr.push(bo.fromTo[0]);
+					for(var i = Math.min(bo.fromTo[0].y, btnObj.y), l = Math.abs(bo.fromTo[0].y - btnObj.y); i < l; ++i){
+						console.log(i+':'+l);
+						if(bo.btnArr[btnObj.x][i] !== btnObj.color){
+							console.log('color is diffrent');
+							$(this).addClass('active').siblings().removeClass('active');
+							bo.fromTo[0] = btnObj;
+							_tempArr = [];
+							break;
+						}else{
+							_tempArr.push(bo.btnArr[btnObj.x][i]);
+						}
+					}
+					if(_tempArr.length){
+						console.log(_tempArr);
+						cutBtn();
+					}else{
+						// return the one btn is selected
+						return;
+					}
+				}else if(bo.fromTo[0].y === btnObj.y){
+
+				}else if(Math.abs(bo.fromTo[0].x - btnObj.x) === Math.abs(bo.fromTo[0].y - btnObj.y)){
+
+				}else{
+					console.log('not a line!');
+					$(this).addClass('active').siblings().removeClass('active');
+					bo.fromTo[0] = btnObj;
+					return;
+				}
+			}else{
+				console.log('color is diffrent!');
+				$(this).addClass('active').siblings().removeClass('active');
+				bo.fromTo[0] = btnObj;
+				return;
+			}
+			
 		}
 	});
 
 	$(window).resize(function(){
-		var _wid = $('.btn').width();
-
-		$('.btn').css('top',_wid);
+		var _wid = $('.btn').width(),
+			_hei = _wid/bo.dimensionHorizon;
+			// _top = _hei * i,
+			// _left = _widPer * j;
+		$('.btn').each(function(i){
+			$(this).css({
+				top : Math.floor(i / bo.dimensionHorizon) * _wid,
+				left : i % bo.dimensionHorizon * _wid
+			})
+		});
 		$('.btn span').height(_wid);
 	})
-})
+});
